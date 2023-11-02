ad4ace9522ea85201cc7fbaac70926ccc8c76224
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-03 00:27:38 +0100

    v0

--- sale_inplast/models/product_template.py
+++ sale_inplast/models/product_template.py
@@ -7,5 +7,5 @@ _logger = logging.getLogger(__name__)
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True, digits=6)
+    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True)
     # falta el campo para el incremento de precio !!! usamos mientras el precio pvp
