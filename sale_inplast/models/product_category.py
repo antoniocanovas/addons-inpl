ea9b1b70363668eca2c258adcbc3487b13a7e33a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-03 00:20:33 +0100

    v0

--- sale_inplast/models/product_category.py
+++ sale_inplast/models/product_category.py
@@ -8,7 +8,7 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     pnt_material_type = fields.Many2one('product.template', store=True, copy=True, domain="[('detailed_type','=','product')]")
-    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True, digits=6)
+    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True)
     pnt_mrp_fault_percent = fields.Float('Fault (%)', store=True, copy=True)
 
 
