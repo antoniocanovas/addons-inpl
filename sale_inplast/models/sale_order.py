d06819c047f710abdedb1e60bfc1154091bf6bc4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-07 12:03:43 +0100

    v1

--- sale_inplast/models/sale_order.py
+++ sale_inplast/models/sale_order.py
@@ -9,12 +9,12 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.onchange('partner_id', 'state')
-    def _avoid_outdated_pricelists(self):
+#    @api.onchange('partner_id', 'state')
+#    def _avoid_outdated_pricelists(self):
 #        today = date.today()
 #        locking_days = self.env.user.company_id.pnt_pricelist_day_lock
 #        lock_date = today - timedelta(days = locking_days)
 #        pricelist_date = self.partner_id.property_product_pricelist.pnt_tracking_date
 #        if not (pricelist_date) or (pricelist_date < lock_date):
 #            raise UserError('Outdate pricelist !!')
-        return True
+#        return True
