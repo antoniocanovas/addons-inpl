e386ead676f0326dc927ef75d3f1e2e6091442e0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-09 01:10:20 +0100

    v1

--- sale_inplast/models/res_partner.py
+++ sale_inplast/models/res_partner.py
@@ -7,19 +7,27 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.onchange('parent_id')
+    def _get_pricelist_mode(self):
+        if self.parent_id.id:
+            self.pnt_pricelist_mode = self.parent_id.pnt_pricelist_mode
     pnt_pricelist_mode   = fields.Selection([('auto','Automático'),
                                              ('bom', 'Escandallo general'),
                                              ('custom', 'Escandallo personalizado')],
-                                            store=True, copy=True)
+                                            store=True, copy=True, compute='_get_pricelist_mode')
 
+    @api.onchange('parent_id')
+    def _get_pricelist_update(self):
+        if self.parent_id.id:
+            self.pnt_pricelist_update = self.parent_id.pnt_pricelist_update
     pnt_pricelist_update = fields.Selection([('1m', 'Monthly'),
                                              ('3m', 'Quarter'),
                                              ('6m', '6 months'),
                                              ('custom', 'Negociación')],
-                                            store=True, copy=True)
+                                            store=True, copy=True, compute='_get_pricelist_update')
 
-    @api.onchange('parent_id')
-    def _get_pricelist_mode_update(self):
-        if self.parent_id.id:
-            self.write({'pnt_pricelist_mode': self.parent_id.pnt_pricelist_mode,
-                        'pnt_pricelist_update': self.parent_id.pnt_pricelist_update})
+#    @api.onchange('parent_id')
+#    def _get_pricelist_mode_update(self):
+#        if self.parent_id.id:
+#            self.write({'pnt_pricelist_mode': self.parent_id.pnt_pricelist_mode,
+#                        'pnt_pricelist_update': self.parent_id.pnt_pricelist_update})
