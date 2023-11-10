451d923c680f248bbab7b5bb544cbd09b624341a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-10 01:34:25 +0100

    v1

--- sale_inplast/models/res_partner.py
+++ sale_inplast/models/res_partner.py
@@ -7,17 +7,14 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.onchange('parent_id')
-    def _get_pricelist_mode(self):
-        if self.parent_id.id:
-            self.pnt_pricelist_mode = self.parent_id.pnt_pricelist_mode
+
     pnt_pricelist_mode   = fields.Selection([('auto','Automático'),
                                              ('bom', 'Escandallo general'),
                                              ('custom', 'Escandallo personalizado')],
-                                            store=True, copy=True, compute='_get_pricelist_mode')
+                                            store=True, copy=True, string='Pricelist mode')
 
     pnt_pricelist_update = fields.Selection([('1m', 'Monthly'),
                                              ('3m', 'Quarter'),
                                              ('6m', '6 months'),
                                              ('custom', 'Negociación')],
-                                            store=True, copy=True)
+                                            store=True, copy=True, string='Pricelist update')
