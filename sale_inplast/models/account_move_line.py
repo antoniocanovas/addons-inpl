c01496406323470f09180afbe7132679ec3ea3c6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-30 17:13:23 +0100

    purchase invoice domain

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -7,15 +7,5 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    @api.depends('move_type', 'product_id')
-    def _get_pnt_product_ids(self):
-        for record in self:
-            if (record.move_type in ['out_invoice','out_refund','out_receipt']):
-                pp = record.move_id.pricelist_id.pnt_product_ids
-            elif (record.move_type in ['in_invoice','in_refund','in_receipt']):
-                pp = self.env['product.product'].search([('purchase_ok','=',True)])
-            else:  # Es 'entry'
-                pp = self.env['product.product'].search([])
-            record['pnt_product_ids'] = [(6, 0, pp.ids)]
     pnt_product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
-                                       compute='_get_pnt_product_ids')
+                                   related='move_id.pricelist_id.pnt_product_ids')
