8940628ff78626af69334af10a4ffc22da7fec3f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-30 16:35:58 +0100

    purchase invoice domain

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -7,9 +7,6 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-#    pnt_product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
-#                                   related='move_id.pricelist_id.pnt_product_ids')
-
     @api.depends('move_type', 'product_id')
     def _get_pnt_product_ids(self):
         for record in self:
