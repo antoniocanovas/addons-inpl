98f76850d23b787233a82466b861904d29334b80
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-01 17:26:35 +0100

    product_type

--- custom_inplast/models/product_template.py
+++ custom_inplast/models/product_template.py
@@ -8,7 +8,13 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
 
-    pnt_product_type = fields.Selection(string='Product type', related='categ_id.pnt_product_type')
+    pnt_product_type = fields.Selection([('final','End-product'),
+                                         ('semi', 'Semi-finished'),
+                                         ('raw', 'Raw'),
+                                         ('dye', 'Dye'),
+                                         ('packing', 'Packing'),
+                                         ('other', 'Other')],
+                                        string='Product type', related='categ_id.pnt_product_type')
     pnt_product_dye_id = fields.Many2one('product.template', string='Product dye', store=True, copy=True)
 
     pnt_product_coa = fields.Many2one(
