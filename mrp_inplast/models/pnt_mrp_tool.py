ee32c799bb9e1f234c8973a48e6ed9bd22215d3e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-01 23:32:45 +0100

    mrp

--- mrp_inplast/models/pnt_mrp_tool.py
+++ mrp_inplast/models/pnt_mrp_tool.py
@@ -4,13 +4,13 @@ from odoo import fields, models, api
 from odoo.exceptions import UserError, ValidationError
 
 
-class PntCoa(models.Model):
-    _name = "pnt.coa"
-    _description = "COA fields"
+class MrpTool(models.Model):
+    _name = "pnt.mrp.tool"
+    _description = "PNT MRP Tools"
 
-    name = fields.Char(
-        string="Name"
-    )
-    pnt_coa_body = fields.Html(
-        string="Body"
-    )
+    name = fields.Char(string="Name", store=True)
+    pnt_type = fields.Selection([('tool','Tool'),('accesory','Accessory'), string='Type', store=True, copy=True])
+    pnt_tool_id = fields.Many2one('pnt.mrp.tool', string='Tool', copy=True, store=True)
+    pnt_cycles = fields.Integer('Cycles / Speed', copy=True, store=True)
+    pnt_gaps = fields.Integer('Gaps', copy=True, store=True)
+    pnt_note = fields.Html(string="Notes")
