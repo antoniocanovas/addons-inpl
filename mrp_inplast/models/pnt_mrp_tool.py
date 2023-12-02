b76ff9f4c120a9dd37d755a1f6f5c4c8e4dedf93
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-02 01:11:25 +0100

    mrp

--- mrp_inplast/models/pnt_mrp_tool.py
+++ mrp_inplast/models/pnt_mrp_tool.py
@@ -10,8 +10,7 @@ class MrpTool(models.Model):
 
     name = fields.Char(string="Name", store=True)
     pnt_type = fields.Selection([('tool','Tool'),('accesory','Accessory')], string='Type', store=True, copy=True)
-    pnt_tool_id = fields.Many2one('pnt.mrp.tool', string='Tool', copy=True, store=True,
-                                  domain=[("pnt_type','=','tool")])
+    pnt_tool_id = fields.Many2one('pnt.mrp.tool', string='Tool', copy=True, store=True)
     pnt_product_id = fields.Many2one('product.template', string='Product', copy=True, store=True)
     pnt_cycles = fields.Integer('Cycles / Speed', copy=True, store=True)
     pnt_gaps = fields.Integer('Gaps', copy=True, store=True)
