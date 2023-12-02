b0618d4278376904913141e421695ad766b33cd0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-02 13:05:55 +0100

    mrp

--- mrp_inplast/models/res_company.py
+++ mrp_inplast/models/res_company.py
@@ -14,7 +14,7 @@ _logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    pnt_mrp_tool_categ = fields.Many2one('maintenance.equipment.category', string='MRP Tool category',
+    pnt_mrp_tool_categ = fields.Many2one('maintenance.equipment.category', string='Tools category',
                                          store=True, help="Injections or pressure Molds")
-    pnt_mrp_accesory_categ = fields.Many2one('maintenance.equipment.category', string='MRP Accesories category',
+    pnt_mrp_accesory_categ = fields.Many2one('maintenance.equipment.category', string='Accesories category',
                                              store=True, help="Accesories for molds")
