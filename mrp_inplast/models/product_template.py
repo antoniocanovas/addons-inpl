aa15e38fc753b58b3d8121f09ec996ec45d53283
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-15 00:43:36 +0100

    cate product

--- mrp_inplast/models/product_template.py
+++ mrp_inplast/models/product_template.py
@@ -9,9 +9,12 @@ class ProductTemplate(models.Model):
     # Datos de empresa de categoría de moldes y accesorios para usar en dominios de equipos:
     def get_tool_categ(self):
         self.pnt_mrp_tool_categ_id = self.env.company.pnt_mrp_tool_categ_id.id
-    pnt_mrp_tool_categ_id = fields.Many2one('maintenance.equipment.category', store=False, compute='get_tool_categ')
+    pnt_mrp_tool_categ_id = fields.Many2one('maintenance.equipment.category', store=False,
+                                            compute='get_tool_categ')
+    def get_accesory_categ(self):
+        self.pnt_mrp_accesory_categ_id = self.env.company.pnt_mrp_accesory_categ_id.id
     pnt_mrp_accesory_categ_id = fields.Many2one('maintenance.equipment.category', store=False,
-                                                compute=lambda self: self.env.company.pnt_mrp_accesory_categ_id.id)
+                                                compute='get_accesory_categ')
 
     # El molde para usar en la fabricación (con o sin accesorio):
     pnt_tool_id = fields.Many2one('maintenance.equipment', string='Mold', store=True, copy=True)
