d02710067d909f35c5e7287552ed321cfa5b9a87
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-29 16:57:21 +0100

    plastic tax

--- sale_inplast/models/account_move.py
+++ sale_inplast/models/account_move.py
@@ -15,11 +15,11 @@ class AccountMove(models.Model):
         show_button = False
         if (self.state not in ['cancel']) and (self.move_type in ['in_invoice','in_refund','out_invoice','out_refund']) and not (self.pnt_move_plastic_tax_id.id):
             for li in self.invoice_line_ids:
-                # Si vendemos o compramos plástico en España, el impuesto va en PVP o ya lo pagó el proveedor:
                 if (li.product_id.pnt_plastic_weight != 0) and (li.quantity != 0):
                     if (self.partner_id.country_id.code != 'ES'):
                         show_button = True
-                    # Si vendemos plástico producido en España en España hemos de pagar (si venta en el extranjero, no):
+                    # Si vendemos o compramos plástico en España, el impuesto va en PVP o ya lo pagó el proveedor:
+                    # Si vendemos plástico PRODUCIDO en España, en España hemos de pagar (si venta en el extranjero, no):
                     else:
                         if (self.partner_id.country_id.code == 'ES') and (li.product_id.pnt_is_manufactured):
                             show_button = True
