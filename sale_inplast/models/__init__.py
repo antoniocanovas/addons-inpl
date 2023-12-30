c371d913a39e4c712c89b5eee0ab90f15bf4b47d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-30 17:41:14 +0100

    plastic tax

--- sale_inplast/models/__init__.py
+++ sale_inplast/models/__init__.py
@@ -1,6 +1,6 @@
 from . import sale_order_line
 from . import product_pricelist
-from . import product_pricelist_item
+#from . import product_pricelist_item
 #from . import sale_order_option
 #from . import account_move_line
 #from . import product_category
