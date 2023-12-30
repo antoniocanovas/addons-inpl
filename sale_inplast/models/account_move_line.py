8cc76c0cac121aa7e6c8a6d1f89692a46d21e17a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-30 16:27:01 +0100

    plastic tax

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -7,5 +7,4 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    pnt_product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
-                                   related='move_id.pricelist_id.pnt_product_ids')
+    pnt_product_ids = fields.Many2many('product.product', store=False, string='Pricelist products'
