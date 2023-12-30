e24ba7152c6297fcdc127a4a50c059b3c073bbb4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-30 10:24:22 +0100

    plastic tax

--- sale_inplast/models/product_category.py
+++ sale_inplast/models/product_category.py
@@ -1,4 +1,5 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -6,13 +7,14 @@ _logger = logging.getLogger(__name__)
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    pnt_raw_material = fields.Many2one('product.template', store=True, copy=True, string='Raw material',
+    pnt_raw_material = fields.Many2one('product.template', store=True, copy=True, string='Raw material', tracking=True,
                                        domain="[('detailed_type','=','product')]",
                                        help='Main component to manufacture these category products.')
-    pnt_plastic_weight = fields.Float('Plastic weight', store=True, copy=True, digits='Stock Weight',
+    pnt_plastic_weight = fields.Float('Plastic weight', store=True, copy=True, digits='Stock Weight', tracking=True,
                                       help='Unit weight used to pricelist recalculation and plastic taxes.')
-    pnt_mrp_fault_percent = fields.Float('Fault (%)', store=True, copy=True,
+    pnt_mrp_fault_percent = fields.Float('Fault (%)', store=True, copy=True, tracking=True,
                                          help='Production percent deficiency')
 
     # Incremento tanto por mil debido a variaciones del coste de materia prima y energía:
@@ -26,3 +28,10 @@ class ProductCategory(models.Model):
 
     pnt_is_manufactured = fields.Boolean('Manufactured', store=True, copy=True, default=True,
                                          help='Enabled if products are manufactured, disabled when bought.')
+
+    @api.constrains('pnt_plastic_weight')
+    def _avoid_change_weight_if_exists_plastic_tax_moves(self):
+        account_moves = self.env['account.move.line'].search([('product_id.categ_id','=', self.id),])
+        if account_moves.ids:
+            raise UserError('You can not change the weight used for previous taxes accounting and sales pricelists,'
+                            'create another category and products.')
