81dbcb4956485b68414bfa3b656c9a54abffe231
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-04 11:45:49 +0100

    plastic tax

--- sale_inplast/models/account_move.py
+++ sale_inplast/models/account_move.py
@@ -275,9 +275,11 @@ class AccountMove(models.Model):
     def _check_plastic_tax_required(self):
         for record in self:
             if (record.move_type in ['in_invoice', 'in_refund']) and (record.state in ['posted']):
+                # Control de que el cliente tiene asignado el país:
                 if (not record.partner_id.country_id.id):
                     raise UserError('Pon el país al proveedor para poder controlar el impuesto al plástico.')
-                elif (record.partner_id.country_id.code != 'ES') and not (record.pnt_move_plastic_tax_id.id):
+                # Si el país es España quien vende ha pagado impuesto y no podemos repercutirlo, si extranjero hemos de pagar:
+                if (record.partner_id.country_id.code != 'ES') and not (record.pnt_move_plastic_tax_id.id):
                     plastic_tax_required = False
                     for li in record.invoice_line_ids:
                         if li.product_id.pnt_plastic_weight != 0:
@@ -285,10 +287,18 @@ class AccountMove(models.Model):
                             raise UserError(message)
 
             if (record.move_type in ['out_invoice', 'out_refund']) and (record.state in ['posted']):
+                # Control de que el cliente tiene asignado el país:
                 if (not record.partner_id.country_id.id):
                     raise UserError('Pon el país al cliente para poder controlar el impuesto al plástico.')
-                elif (record.partner_id.country_id.code != 'ES') and not (record.pnt_move_plastic_tax_id.id):
+                # Si es cliente extranjero y el plástico fue importado pagando tasas, podemos recuperar el importe:
+                if (record.partner_id.country_id.code != 'ES') and not (record.pnt_move_plastic_tax_id.id):
                     for li in record.invoice_line_ids:
                         if (li.product_id.pnt_plastic_weight != 0) and (li.product_id.categ_id.pnt_is_manufactured == False):
                             message = "El producto " + li.product_id.name + " es susceptible de recuperar el impuesto al plástico, crea o asigna el apunte correspondiente en esta factura"
+                            raise UserError(message)
+                # Caso de venta en España de plástico fabricado por nosotros en España, requiere impuesto:
+                if (record.partner_id.country_id.code == 'ES') and not (record.pnt_move_plastic_tax_id.id):
+                    for li in record.invoice_line_ids:
+                        if (li.product_id.pnt_plastic_weight != 0) and (li.product_id.categ_id.pnt_is_manufactured == True):
+                            message = "El producto " + li.product_id.name + " requiere impuesto al plástico, crea o asigna el apunte correspondiente en esta factura"
                             raise UserError(message)
