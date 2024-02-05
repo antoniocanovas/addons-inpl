8a1a620ebbf1347a75820fd050f77d12198b021e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-02-05 01:35:06 +0100

    mrp inplast

--- mrp_inplast/models/mrp_bom.py
+++ mrp_inplast/models/mrp_bom.py
@@ -27,9 +27,9 @@ class MrpBom(models.Model):
         for record in self:
             qty = 0
             factor = record.product_uom_id._compute_quantity(record.product_qty, record.product_tmpl_id.uom_id)
-            if record.pnt_percent_distribution_type == 'weight':
+            if record.pnt_raw_type == 'weight':
                 qty = record.product_id.weight * factor
-            if record.pnt_percent_distribution_type == 'weight':
+            if record.pnt_raw_type == 'weight':
                 qty = record.product_id.volume * factor
             record['pnt_product_distribution_qty'] = qty
     pnt_raw_qty = fields.Float('UOM Qty', store=True, compute='_get_product_raw_qty')
