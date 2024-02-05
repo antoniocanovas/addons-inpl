cb3f0e022fd5d26ea36c345462fe253eea32c1d8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-02-05 02:35:51 +0100

    mrp inplast

--- mrp_inplast/models/mrp_bom.py
+++ mrp_inplast/models/mrp_bom.py
@@ -22,22 +22,14 @@ class MrpBom(models.Model):
     )
 
     # Cantidad de producto (peso o volumen) a distribuir entre productos de esta categoría según product.template:
-    @api.depends('product_tmpl_id', 'product_uom_id', 'product_tmpl_id.weight', 'product_tmpl_id.volume')
+    @api.depends('product_tmpl_id', 'product_uom_id', 'product_tmpl_id.weight', 'product_tmpl_id.volume', 'product_qty')
     def _get_product_raw_qty(self):
         for record in self:
             qty = 0
             factor = record.product_uom_id._compute_quantity(record.product_qty, record.product_tmpl_id.uom_id)
             if record.pnt_raw_type == 'weight':
-                qty = record.product_tmpl_id.weight * factor
+                qty = record.product_tmpl_id.weight * factor * record.product_qty
             if record.pnt_raw_type == 'weight':
-                qty = record.product_tmpl_id.volume * factor
+                qty = record.product_tmpl_id.volume * factor * record.product_qty
             record['pnt_raw_qty'] = qty
     pnt_raw_qty = fields.Float('UOM Qty', store=True, compute='_get_product_raw_qty')
-
-    @api.depends('pnt_raw_type')
-    def _get_percent_hide(self):
-        hide = True
-        if self.pnt_raw_type: hide = False
-        self.pnt_percent_hide = hide
-    pnt_percent_hide = fields.Boolean('Hide percent', compute='_get_percent_hide')
-
--- mrp_inplast/models/mrp_bom_line.py
+++ mrp_inplast/models/mrp_bom_line.py
@@ -15,10 +15,3 @@ class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
     pnt_raw_percent = fields.Float('Percent')
-
-    @api.depends('bom_id.pnt_raw_type')
-    def _get_percent_hide(self):
-        hide = True
-        if self.bom_id.pnt_raw_type: hide = False
-        self.pnt_percent_hide = hide
-    pnt_percent_hide = fields.Boolean('Hide percent', compute='_get_percent_hide')
