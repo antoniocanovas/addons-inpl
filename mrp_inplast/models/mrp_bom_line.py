2cd95821e9adde121a0c6c53c137850d21786283
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-02-07 12:33:55 +0100

    bompercent

--- mrp_inplast/models/mrp_bom_line.py
+++ mrp_inplast/models/mrp_bom_line.py
@@ -15,11 +15,24 @@ class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
     pnt_raw_percent = fields.Float('Percent')
+    pnt_raw_type_id = fields.Many2one(related='bom_id.pnt_raw_type_id')
 
-    @api.depends('pnt_raw_percent')
+    @api.onchange('pnt_raw_percent','product_id')
     def _get_units_from_total_percent(self):
-        for record in self:
-            # Faalta el if de que sea la misma clase de unidad y asginar la misma que del peso o volumen:
-            qty = record.bom_id.pnt_raw_qty * record.pnt_raw_percent / 100
-            uom = record.bom_id.product_tmpl_id.uom_id
-            record.write({'product_qty': qty, 'product_uom_id': uom.id})
+        qty = self._origin.product_qty
+        if (self.pnt_raw_percent != 0) and (self.pnt_raw_type_id == self.product_uom_category_id):
+            qty = self.bom_id.pnt_raw_qty * self.pnt_raw_percent / 100
+        self.product_qty = qty
+    product_qty = fields.Float(compute='_get_units_from_total_percent')
+
+    @api.onchange('pnt_raw_percent','product_id')
+    def _get_uom_from_percent_type(self):
+        # Falta el if de que sea la misma clase de unidad y asginar la misma que del peso o volumen:
+        uom = self._origin.product_uom_id
+        if (self.pnt_raw_percent != 0) and (self.pnt_raw_type_id == self.product_uom_category_id):
+            uom = self.env['uom.uom'].search([
+                ('category_id','=',self.product_uom_category_id.id),
+                ('uom_type','=','reference')
+            ])
+        self.product_uom_id = uom.id
+    product_uom_id = fields.Many2one(compute='_get_uom_from_percent_type')
