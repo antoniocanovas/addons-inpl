0f1bcb38dff9ee8bf5bb82055756b9834dbe2541
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-02 01:39:45 +0200

    r3

--- product_inplast/models/product_packing_wizard.py
+++ product_inplast/models/product_packing_wizard.py
@@ -95,7 +95,7 @@ class ProductPackingWizard(models.TransientModel):
 
             # Crear componentes de la lista de materiales para CAJAS:
             if record.pnt_type == 'box':
-                product = self.env['product.product'].search([('product_tmpl_id','=', record.id)])
+                product = self.env['product.product'].search([('product_tmpl_id','=', name.id)])
                 newbomboxline  = self.env['mrp.bom.line'].create(
                     {'product_id': product.id, 'product_qty': record.pnt_box_base_qty, 'bom_id': newldm.id })
                 boxproduct = self.env['product.product'].search([('product_tmpl_id','=', record.pnt_box_type_id.id)])[0]
