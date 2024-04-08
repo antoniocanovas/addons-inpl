3b80317fbeab6811bd8d006e4490b11fb0d604b4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-08 17:05:11 +0200

    prefix view

--- l10n_es_aeat_mod592_pnt/models/product_template.py
+++ l10n_es_aeat_mod592_pnt/models/product_template.py
@@ -11,11 +11,22 @@ class ProductTemplate(models.Model):
 
     pnt_is_manufactured = fields.Boolean('Manufacture', store=True, related='categ_id.pnt_is_manufactured')
 
+
+    # Impuesto del plástico, el cliente solicita importe por millar para usar en pequeños formatos:
     @api.onchange('categ_id','pnt_plastic_weight', 'pnt_is_manufactured')
     def _get_plastic_unit_tax(self):
+        for record in self:
+            tax = self.env.company.pnt_plastic_tax
+            record['pnt_plastic_tax'] = tax * record.pnt_plastic_weight
+    pnt_plastic_tax = fields.Float('Plastic tax', store=False,
+                                            digits='Product Price',
+                                            compute='_get_plastic_unit_tax')
+
+    @api.onchange('categ_id','pnt_plastic_weight', 'pnt_is_manufactured')
+    def _get_plastic_1000unit_tax(self):
         for record in self:
             tax = self.env.company.pnt_plastic_tax
             record['pnt_plastic_1000unit_tax'] = tax * record.pnt_plastic_weight * 1000
     pnt_plastic_1000unit_tax = fields.Float('Plastic tax x 1000', store=False,
                                             digits='Product Price',
-                                            compute='_get_plastic_unit_tax')
+                                            compute='_get_plastic_1000unit_tax')
