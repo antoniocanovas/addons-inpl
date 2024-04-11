467dba54128fc71801d140f93d5d3558c8f16744
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-11 03:54:35 +0200

    recalc bomline

--- mrp_inplast/models/mrp_bom_line.py
+++ mrp_inplast/models/mrp_bom_line.py
@@ -17,17 +17,18 @@ class MrpBomLine(models.Model):
     pnt_raw_percent = fields.Float('Percent')
     pnt_raw_type_id = fields.Many2one(related='bom_id.pnt_raw_type_id')
     product_qty = fields.Float(digits='Stock Weight', compute="_get_product_qty")
-    bom_product_qty = fields.Float(related='bom_id.product_qty')
+ #   bom_product_qty = fields.Float(related='bom_id.product_qty')
 
-    @api.onchange('pnt_raw_percent','bom_product_qty')
+    @api.onchange('pnt_raw_percent','bom_id.bom_product_qty')
     def _get_product_qty(self):
         for record in self:
-            qty = record.product_qty
-            if (record.pnt_raw_percent != 0) and (
-                    record.pnt_raw_type_id == record.product_uom_category_id):
-                qty = record.bom_id.pnt_raw_qty * record.pnt_raw_percent / 100
+            if record.pnt_raw_type_id.id:
+                qty = record.product_qty
+                if (record.pnt_raw_percent != 0) and (
+                        record.pnt_raw_type_id == record.product_uom_category_id):
+                    qty = record.bom_id.pnt_raw_qty * record.pnt_raw_percent / 100
 
-            record.write({'product_qty': qty})
+                record.write({'product_qty': qty})
 
     @api.onchange('pnt_raw_percent','product_id')
     def _get_uom_from_percent_type(self):
