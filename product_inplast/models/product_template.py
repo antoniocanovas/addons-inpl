84e0c797e61652f625b774557698a9f7ac36c4a0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-07 18:29:36 +0200

    digits xml

--- product_inplast/models/product_template.py
+++ product_inplast/models/product_template.py
@@ -46,4 +46,4 @@ class ProductTemplate(models.Model):
     pnt_packing_ids = fields.One2many('product.template', 'pnt_parent_id', string='Packing products')
     pnt_bom_line_ids = fields.One2many('mrp.bom.line', 'product_tmpl_id', string='BOM lines')
 
-    standard_price = fields.Float(digits='INPLAST Cost')
+    standard_price = fields.Float(string="Coste inplast", digits='INPLAST Cost')
