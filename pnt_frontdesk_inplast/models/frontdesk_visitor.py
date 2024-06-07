107907a66a073bc8f42b194fbc33aed5e6c99887
arquero20022 <60667813+arquero20022@users.noreply.github.com>
2024-06-07 12:39:07 +0200

    tarjetas actualizadas

--- pnt_frontdesk_inplast/models/frontdesk_visitor.py
+++ pnt_frontdesk_inplast/models/frontdesk_visitor.py
@@ -6,5 +6,5 @@ from odoo import api, fields, models, _
 class FrontdesckVisitor(models.Model):
     _inherit = "frontdesk.visitor"
 
-    security_level = fields.Selection([('fabric','Fabric'),('office','Office')], string= 'Security level')
+    security_level = fields.Selection([('visitor','Visitor'),('external','External')], string= 'Security level')
 
