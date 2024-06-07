f1e39636c696222c8eb3a50bdae0a25d5110a28f
arquero20022 <60667813+arquero20022@users.noreply.github.com>
2024-06-07 11:33:51 +0200

    Delete pnt_frontdesk directory

--- pnt_frontdesk/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import frontdesk_visitor
--- pnt_frontdesk/models/frontdesk_visitor.py
+++
@@ -1,9 +0,0 @@
-
-from odoo import api, fields, models, _
-
-
-
-class FrontdesckVisitor(models.Model):
-    _inherit = "frontdesk.visitor"
-
-    security_level = fields.Selection([('fabric','Fabric'),('office','Office')], string= 'Security level')
