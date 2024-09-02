14e20d1e69d1685474aab9ea14b71a87a4e00978
amunoz <amunoz@puntsistemes.es>
2024-09-02 13:39:39 +0000

    mrp boxes in workorder

---
+++ mrp_inplast/models/mrp_workorder.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import base64
+from datetime import datetime
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, models, _, api
+from odoo.exceptions import UserError
+from odoo.tools import file_open
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.workorder'
+
+    def add_pallet_boxes(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'pallet.boxes.wizard',
+            'views': [[self.env.ref('mrp_inplast.view_add_pallet_boxes_wizard').id, 'form']],
+            'name': _('Add Boxes'),
+            'target': 'new',
+            'context': {
+                'production_id': self.id,
+            }
+        }
+        # Sobreescribe la función de mrp_lot_as_serial para tener en cuenta los lotes de cajas documentadas al fabricar palet:
+
+    def update_lot_as_serial(self):
+        # Son stock.move, buscamos sólo los que son tipo packing = palet del campo raw_move_ids:
+        sms = self.env['stock.move'].search(
+            [('id', 'in', self.move_raw_ids.ids), ('product_id.pnt_product_type', '=', 'packing')])
+        boxeslot = []
+        for sm in sms:
+            if sm.move_line_ids.lot_id.related_boxes_ids:
+                for li in sm.move_line_ids.lot_id.related_boxes_ids:
+                    boxeslot.append(li.id)
+
+        if boxeslot == []:
+            super().update_lot_as_serial()
+        else:
+            # Comprobar el número de lotes origen y destino, si no coinciden mensaje de error.
+            # Asignar los lotes que ya existen a las cajas desmontadas.
+            if len(boxeslot) != len(self.finished_move_line_ids):
+                raise UserError(_("No coincide el número de cajas con los lotes disponibles!"))
+            else:
+                i = 0
+                for li in self.finished_move_line_ids:
+                    li.write({'lot_id': boxeslot[i]})
+                    i += 1
+
