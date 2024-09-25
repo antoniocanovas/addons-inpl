1fb33903bdde406ddd6d1a67a91e5cf2dd906148
arquero20022 <amm2002ct@gmail.com>
2024-09-25 15:30:25 +0000

    error numero sscc

--- mrp_inplast/models/stock_picking.py
+++ mrp_inplast/models/stock_picking.py
@@ -7,7 +7,7 @@ class StockPicking(models.Model):
     def add_sscc(self):
         partner = self.partner_id or self.parent_id.partner_id  # Obtener el partner
 
-        for line in self.move_line_ids:
+        for line in range(self.move_line_ids):
             for sscc in partner.sscc_qty:
 
                 line.lot_id.get_next_sscc()
