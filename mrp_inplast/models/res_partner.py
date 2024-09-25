9e0abbc6e899867f56656c704e04b8109fefae0d
arquero20022 <amm2002ct@gmail.com>
2024-09-25 15:10:10 +0000

    error numero sscc

--- mrp_inplast/models/res_partner.py
+++ mrp_inplast/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    number_of_sscc = fields.Integer(string="Numbers of SSCC")
+    sscc_qty = fields.Integer(string="Numbers of SSCC")
--- mrp_inplast/models/stock_picking.py
+++ mrp_inplast/models/stock_picking.py
@@ -8,7 +8,7 @@ class StockPicking(models.Model):
         partner = self.partner_id or self.parent_id.partner_id  # Obtener el partner
 
         for line in self.move_line_ids:
-            for sscc in partner.number_sscc:
+            for sscc in partner.sscc_qty:
 
                 line.lot_id.get_next_sscc()
 
