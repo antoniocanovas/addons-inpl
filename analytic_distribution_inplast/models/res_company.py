014fc19b8aa579bb5c134c16a5ae40d242c5bae4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-09 18:53:00 +0200

    analytic distribution modes

--- analytic_distribution_inplast/models/res_company.py
+++ analytic_distribution_inplast/models/res_company.py
@@ -7,6 +7,6 @@ from odoo import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    analytic_product_plan = fields.Many2one('account.analytic.plan', string='Product analytic plan')
-    analytic_categ_plan = fields.Many2one('account.analytic.plan', string='Category analytic plan')
-    analytic_department_plan = fields.Many2one('account.analytic.plan', string='Department analytic plan')
+    analytic_product_plan = fields.Many2one('account.analytic.plan', string='Product')
+    analytic_categ_plan = fields.Many2one('account.analytic.plan', string='Category')
+    analytic_department_plan = fields.Many2one('account.analytic.plan', string='Department')
