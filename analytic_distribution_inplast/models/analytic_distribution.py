8520d65ed7fe93032aa52f697febb86e61f19b8d
arquero20022 <amm2002ct@gmail.com>
2024-10-09 14:49:27 +0000

    r13 r14

--- analytic_distribution_inplast/models/analytic_distribution.py
+++ analytic_distribution_inplast/models/analytic_distribution.py
@@ -1,67 +1,174 @@
-# Copyright
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-
 from odoo import fields, models, api
-from datetime import datetime
 from odoo.exceptions import UserError
 
+
 class AnalyticDistribution(models.Model):
-    _inherit = 'analytic.distribution'
+    _inherit = "analytic.distribution"
 
-    compute_mode = fields.Selection([
-        ('demo','demo INPLAST'),
-        ('r13', 'R13.- Electricidad'),
-    ])
+    compute_mode = fields.Selection(
+        [
+            ("demo", "demo INPLAST"),
+            ("r13", "R13.- Electricidad"),
+            ("r14", "Reparto 14"),
+        ]
+    )
 
-    workcenter_ids = fields.Many2many('mrp.workcenter', string="Workcenters")
-
+    workcenter_ids = fields.Many2many("mrp.workcenter", string="Workcenters")
 
     def compute_distribution(self):
-        """ Extend this function with custom Inplast analytic compute modes
-        """
+        """Extend this function with custom Inplast analytic compute modes"""
         super().compute_distribution()
+        self.env["account.analytic.line"].search(
+            [("analytic_distribution_id", "=", self.id)]
+        ).unlink()
         self.inplast_computed_modes()
 
     def inplast_computed_modes(self):
-        if self.compute_mode == 'demo':
-            raise UserError('ok')
-        elif self.compute_mode == 'r13':
+        if self.compute_mode == "demo":
+            raise UserError("ok")
+        elif self.compute_mode == "r13":
             self.compute_r13()
+        elif self.compute_mode == "r14":
+            self.compute_r14()
 
     def compute_r13(self):
         datefrom = self.date_from
         dateto = self.date_to
-        total_kw, mrpproducts = 0, []
-        # Array de máquinas a considerar:
+        total_kwh = 0  # Total de kWh consumidos por todas las máquinas
         workcenters = self.workcenter_ids
-        # Manufacturing order consideradas entre fechas:
-        workorders = self.env['mpr.workorder'].search(
-            [('workcenter_id','in',workcenter.ids),
-             ('date_start', '>=', datefrom),
-             ('date_start', '<=', dateto)])
+        amount = self.amount  # El máximo coste a distribuir
 
-        # Cálculo del total de kw en todos los workcenter en base al tiempo trabajado:
+        # Órdenes de manufactura consideradas entre fechas:
+        workorders = self.env["mrp.workorder"].search(
+            [
+                ("workcenter_id", "in", workcenters.ids),
+                ("date_start", ">=", datefrom),
+                ("date_start", "<=", dateto),
+            ]
+        )
+
+        # Inicialización de listas simples
+        mrpproducts = []
+        product_total_kwh = []
+
+        # Cálculo del total de kWh consumidos
         for wo in workorders:
-            total_kw += wo.duration * wo.workcenter_id.power_kw
-            if wo.product_id not in mainproducts:
-                mainproducts.append(wo.product_id)
+            product = wo.product_id
+            duration = wo.duration
+            machine = wo.workcenter_id
 
-        # Bucle para recorrer las operaciones de cada producto entre fechas:
-        # (consumo teórico por máquina, consumo teórico total, tapones por máquina, tapones total):
-        for product in mrpproducts:
-            power = 0
-            productworkorders = self.env['mrp.workorder'].search(
-                [('workcenter_id','in',workcenter.ids),
-                 ('date_start', '>=', datefrom),
-                 ('date_start', '<=', dateto),
-                 ('product_id','=',product.id)])
-            for pwo in productworkorders:
-                # Consumo:
-                power += 1
-                # Nº tapones:
+            # Identificamos productos únicos y agregamos a la lista si no están
+            if product not in mrpproducts:
+                mrpproducts.append(product)
+                product_total_kwh.append(0)  # Inicializamos su consumo total a 0
 
-                # Búsqueda de cuenta analítica, creación si no existe:
+            # Calculamos el consumo de kWh
+            kwh_consumed = duration * machine.power_kw
+            total_kwh += kwh_consumed
 
-                # Creación de la imputación:
+            # Actualizamos el consumo total por producto
+            product_index = mrpproducts.index(product)
+            product_total_kwh[product_index] += kwh_consumed
 
+        # Verificar si hay consumo total de kWh para evitar la división por cero
+        if total_kwh == 0:
+            raise UserError("No hay consumo de energía registrado.")
+
+        # Crear o actualizar entradas analíticas para cada producto
+        for i in range(len(mrpproducts)):
+            product = mrpproducts[i]
+            product_kwh = product_total_kwh[i]
+
+            machine_percentage = (product_kwh / total_kwh) * 100
+            machine_cost = (amount * machine_percentage) / 100
+
+            existing_line = self.env["account.analytic.line"].search(
+                [
+                    ("product_id", "=", product.id),
+                    ("date", ">=", datefrom),
+                    ("date", "<=", dateto),
+                ],
+                limit=1,
+            )
+
+            self.env["account.analytic.line"].create(
+                {
+                    "name": f"Consumo {product.name}",
+                    "amount": -machine_cost,
+                    "product_id": product.id,
+                    "date": fields.Date.today(),
+                    "analytic_distribution_id": self.id,
+                }
+            )
+
+        return True
+
+    def compute_r14(self):
+        datefrom = self.date_from
+        dateto = self.date_to
+        total_duration = 0  # Total de kWh consumidos por todas las máquinas
+        workcenters = self.workcenter_ids
+        amount = self.amount  # El máximo coste a distribuir
+
+        # Órdenes de manufactura consideradas entre fechas:
+        workorders = self.env["mrp.workorder"].search(
+            [
+                ("workcenter_id", "in", workcenters.ids),
+                ("date_start", ">=", datefrom),
+                ("date_start", "<=", dateto),
+            ]
+        )
+
+        # Inicialización de listas simples
+        mrpproducts = []
+        product_total_duration = []
+
+        # Cálculo del total de kWh consumidos
+        for wo in workorders:
+            product = wo.product_id
+            duration = wo.duration
+            machine = wo.workcenter_id
+
+            # Identificamos productos únicos y agregamos a la lista si no están
+            if product not in mrpproducts:
+                mrpproducts.append(product)
+                product_total_duration.append(0)  # Inicializamos su consumo total a 0
+
+            total_duration += duration
+
+            # Actualizamos el consumo total por producto
+            product_index = mrpproducts.index(product)
+            product_total_duration[product_index] += duration
+
+        # Verificar si hay consumo total de kWh para evitar la división por cero
+        if total_duration == 0:
+            raise UserError("No hay consumo de energía registrado.")
+
+        # Crear o actualizar entradas analíticas para cada producto
+        for i in range(len(mrpproducts)):
+            product = mrpproducts[i]
+            product_kwh = product_total_duration[i]
+
+            machine_percentage = (product_kwh / total_duration) * 100
+            machine_cost = (amount * machine_percentage) / 100
+
+            existing_line = self.env["account.analytic.line"].search(
+                [
+                    ("product_id", "=", product.id),
+                    ("date", ">=", datefrom),
+                    ("date", "<=", dateto),
+                ],
+                limit=1,
+            )
+
+            self.env["account.analytic.line"].create(
+                {
+                    "name": f"Consumo {product.name}",
+                    "amount": -machine_cost,
+                    "product_id": product.id,
+                    "date": fields.Date.today(),
+                    "analytic_distribution_id": self.id,
+                }
+            )
+
+        return True
