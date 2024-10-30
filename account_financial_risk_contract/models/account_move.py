d5d4993199af21d21c8841fc291897936a58ff5b
pedroguirao <pguirao@puntsistemes.es>
2024-10-30 16:26:40 +0000

    risk_contract

--- account_financial_risk_contract/models/account_move.py
+++ account_financial_risk_contract/models/account_move.py
@@ -5,29 +5,6 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     risk_batch_id = fields.Many2one(
-        comodel_name="risk.batch",
-        string="Risk batch",
-        store=True,
-        copy=False,
+        "risk.batch", string="Risk batch", store=True, copy=False
     )
-
-    @api.depends("payment_state", "state", "risk_batch_id", "partner_id.risk_total")
-    def _get_risk_communication_pending(self):
-        for record in self:
-            pending = True
-            if record.payment_state not in ["not_paid", "in_payment", "partial"]:
-                pending = False
-            if record.state not in ["posted"]:
-                pending = False
-            if record.risk_batch_id.id:
-                pending = False
-            if record.partner_id.credit_limit == 0:
-                pending = False
-            record.risk_pending = pending
-
-    risk_pending = fields.Boolean(
-        "Risk pending",
-        store=True,
-        default=True,
-        compute="_get_risk_communication_pending",
-    )
+    pnt_credit_limit = fields.Float(related="partner_id.credit_limit")
