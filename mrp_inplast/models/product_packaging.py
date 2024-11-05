2298cc0af4865d30e024face17f811f25e077ee7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-05 15:50:02 +0100

    v3 bom template

--- mrp_inplast/models/product_packaging.py
+++ mrp_inplast/models/product_packaging.py
@@ -3,4 +3,4 @@ from odoo import _, api, fields, models
 class ProductPacking(models.Model):
     _inherit = "product.packaging"
 
-    mrp_bom_template_id = fields.Many2one("BOM template")
+    mrp_bom_template_id = fields.Many2one("product.bom.template", string="BOM template")
