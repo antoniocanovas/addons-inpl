38b87c9b380ec28ec25b12c9af64977b847499d6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 20:24:20 +0100

    logistic inplast

--- logistic_inplast/models/container_type_line.py
+++ logistic_inplast/models/container_type_line.py
@@ -1,9 +1,10 @@
 from odoo import _, api, fields, models
 
-class ContainerType(models.Model):
+class ContainerTypeLine(models.Model):
     _name = "container.type.line"
     _description = "Container type line"
 
     name = fields.Many2one("container.type", string="Name")
     sequence = fields.Integer('Sequence')
     partner_id = fields.Many2one('res.partner', string="Partner")
+    container_type_id = fields.Many2one('container.type', string="Container")
