5b609c364d6d76141315b229996f9a71cc58e727
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 18:08:24 +0100

    logistic inplast

--- logistic_inplast/models/res_partner.py
+++ logistic_inplast/models/res_partner.py
@@ -7,4 +7,4 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    container_type_ids = fields.One2many('contanier.type', 'partner_id', string="Containers")
+    container_type_ids = fields.One2many('container.type', 'partner_id', string="Containers")
