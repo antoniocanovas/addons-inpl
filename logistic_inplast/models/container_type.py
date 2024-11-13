17b48e43bfa6ddbe2ea8882ffaa5b733a0c61331
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 23:47:21 +0100

    logistic inplast

--- logistic_inplast/models/container_type.py
+++ logistic_inplast/models/container_type.py
@@ -4,7 +4,8 @@ class ContainerType(models.Model):
     _name = "container.type"
     _description = "Container type"
 
-    name = fields.Char("Name")
+    name = fields.Char("Name", related='container_type_id.name')
+    container_type_id = fields.Many2one('container.type', string='Type')
     type = fields.Selection(
         [
             ("truck", "Truck"),
