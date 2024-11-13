bfb9715537a6aae947aa3d6d7cd2b27e48af3789
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 23:23:54 +0100

    logistic inplast

--- logistic_inplast/__manifest__.py
+++ logistic_inplast/__manifest__.py
@@ -13,9 +13,12 @@ Gestión de envíos y compras en contenedores y camiones.
         'stock',
         'sale_stock',
         'sale_management',
+        'custom_inplast',
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/menu_views.xml',
+        'views/container_type_views.xml',
         'views/res_partner_views.xml',
         'views/sale_order_views.xml',
         'views/stock_picking_views.xml',
