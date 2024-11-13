90c43d032d063086212fbc4dfb2a466c99ffdc7e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 20:29:45 +0100

    logistic inplast

--- logistic_inplast/models/container_type_line.py
+++ logistic_inplast/models/container_type_line.py
@@ -4,7 +4,7 @@ class ContainerTypeLine(models.Model):
     _name = "container.type.line"
     _description = "Container type line"
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', related="container_type_id.name")
     container_type_id = fields.Many2one("container.type", string="Name")
     sequence = fields.Integer('Sequence')
     partner_id = fields.Many2one('res.partner', string="Partner")
