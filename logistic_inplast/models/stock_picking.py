397765018f540b06e71093c84adca0b745208fb7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-13 20:07:02 +0100

    logistic inplast

--- logistic_inplast/models/stock_picking.py
+++ logistic_inplast/models/stock_picking.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    container_type_id = fields.Many2one(related='sale_id.container_type_id')
+    container_type_id = fields.Many2one('container.type', related='sale_id.container_type_id')
