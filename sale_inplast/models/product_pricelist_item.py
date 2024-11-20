c6339c91823724b363feeedc68e1f7588b29fd25
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-11-20 02:19:55 +0100

    auto recalc packing prices

--- sale_inplast/models/product_pricelist_item.py
+++ sale_inplast/models/product_pricelist_item.py
@@ -11,9 +11,16 @@ class ProductPricelistItem(models.Model):
     pnt_new_price = fields.Float('New price', store=True, copy=False, digits=(3,6))
     pnt_product_state = fields.Boolean('Active', related='product_tmpl_id.active', store=False)
 
-    ### Eliminado 20/05/24, uso de módulos OCA (sigaus)
-    # Al crear la línea asigna el precio del impuesto, aunque aún no se haya actualizado la tarifa nunca:
-    # @api.onchange('product_id','product_tmpl_id')
-    # def _update_plastic_tax(self):
-    #    if self.pricelist_id.pnt_plastic_tax:
-    #        self.price_surcharge = self.product_tmpl_id.pnt_plastic_1000unit_tax / 1000
+    # Campos para poner como sólo lectura los precios de productos "packing" en vistas y recalcular precios:
+    pnt_product_type = fields.Selection(related='product_tmpl_id.pnt_product_type')
+    pnt_parent_id = fields.Many2one('product.template', related='product_tmpl_id.pnt_parent_id')
+
+    def update_packing_products_price(self):
+        for record in self:
+            if record.pnt_product_type == 'final':
+                packing_lines = self.env['product.pricelist.item'].search([
+                    ('pricelist_id','=',record.pricelist_id.id),
+                    ('pnt_parent_id','=',record.id),
+                ])
+                for li in packing_lines:
+                    li['fixed_price'] = record.fixed_price * li.product_tmpl_id.pnt_parent_qty
