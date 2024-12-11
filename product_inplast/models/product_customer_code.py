ddc2781328fbf53c8941fa2998e36f5412dc0fb7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-11 16:41:09 +0100

    customer_code_ids

--- product_inplast/models/product_customer_code.py
+++ product_inplast/models/product_customer_code.py
@@ -5,6 +5,6 @@ class ProductCustomerCode(models.Model):
     _name = "product.customer.code"
     _description = "Product Customer Code"
 
-    name = fields.Char(string="Name", required=1)
+    name = fields.Char(string="Code", required=1)
     partner_id = fields.Many2one('res.partner', string="Customer", required=1)
     product_tmpl_id = fields.Many2one('product.template', string="Product")
