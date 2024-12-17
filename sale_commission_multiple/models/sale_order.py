c9b2e4ef9dcd2a66ff3b29a99957d954b8d0d3fe
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 18:28:58 +0100

    scm

--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -8,7 +8,8 @@ class SaleOrder(models.Model):
     def _get_partner_referrers(self):
         for record in self:
             lines = []
-            if record.partner_id.referrer_plan_ids.ids:
+            # Creación de líneas de comisionistas desde el partner:
+            if record.partner_id.referrer_plan_ids.ids and record.id:
                 for li in record.partner_id.referrer_plan_ids:
                     newline = self.env['referrer.plan.rel'].create({
                         'referrer_id': li.referrer_id.id,
@@ -18,4 +19,21 @@ class SaleOrder(models.Model):
                     lines.append(newline.id)
             record['referrer_plan_ids'] = [(6,0,lines)]
     referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers', store=True,
-                                        compute='_get_partner_referrers')
+                                        compute='_get_partner_referrers'
+                                        )
+
+    """
+    def _create_invoices(self):
+        super()._create_invoices()
+        for record in self:
+            lines = []
+            if record.referrer_plan_ids.ids:
+                for li in record.referrer_plan_ids:
+                    newline = self.env['referrer.plan.rel'].create({
+                        'referrer_id': li.referrer_id.id,
+                        'commission_plan_id': li.commission_plan_id.id,
+                        'invoice_id': nuevafactura.id,
+                    })
+                    lines.append(newline.id)
+            record['referrer_plan_ids'] = [(6,0,lines)]
+    """
