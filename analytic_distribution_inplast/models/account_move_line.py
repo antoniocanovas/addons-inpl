42230291395b44d824237a0f862b25996ea78095
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-10 13:57:33 +0100

    analytic dist v2

--- analytic_distribution_inplast/models/account_move_line.py
+++ analytic_distribution_inplast/models/account_move_line.py
@@ -4,11 +4,17 @@
 
 from odoo import fields, models, api
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
 
     analytic_distribution_account_id = fields.Many2one('account.analytic.account', string='Distribution',
                                                        help='Analytic distribution account')
+    analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan',
+                                                    compute='_get_analytic_distribution_plan')
+
+    def _get_analytic_distribution_plan(self):
+        self.analytic_distribution_plan_id = self.env.company.analytic_distribution_plan_id.id
+
 
     # Eliminado tras reunión con Ibarra (enero 2025):
     #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
