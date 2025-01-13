d566c2d54c173ccc35aac08e14992ee5691cb09f
amunozmt <amunoz@puntsistemes.es>
2025-01-13 15:54:08 +0000

    actualizacion

--- product_inplast/models/product_customer_code.py
+++ product_inplast/models/product_customer_code.py
@@ -5,6 +5,6 @@ class ProductCustomerCode(models.Model):
     _name = "product.customer.code"
     _description = "Product Customer Code"
 
-    name = fields.Char(string="Code", required=1)
-    partner_id = fields.Many2one('res.partner', string="Customer", required=1)
+    name = fields.Char(string="Code", required=True)
+    partner_id = fields.Many2one('res.partner', string="Customer", required=True)
     product_tmpl_id = fields.Many2one('product.template', string="Product")
