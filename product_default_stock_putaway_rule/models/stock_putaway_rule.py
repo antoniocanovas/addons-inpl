5ca2efdf7e5a1110413d78e909d36da6bb8315e9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-16 13:12:16 +0100

    bug spr

--- product_default_stock_putaway_rule/models/stock_putaway_rule.py
+++ product_default_stock_putaway_rule/models/stock_putaway_rule.py
@@ -8,7 +8,10 @@ class StockPutawayRule(models.Model):
 
     name = fields.Char('Name', compute='_get_name')
     def _get_name(self):
-        name = ""
-        if self.product_id.id and self.location_in_id.id and self.location_out_id.id:
-            name = self.product_id.name + " From: " + self.location_in_id.name + " to: " + self.location_out_id.name
-        self.name = name
+        for record in self:
+            name = ""
+            if record.id:
+                name = "From: " + record.location_in_id.name + " to: " + record.location_out_id.name
+            if record.product_id.id:
+                name = record.product_id.name + " " + name
+            record['name'] = name
