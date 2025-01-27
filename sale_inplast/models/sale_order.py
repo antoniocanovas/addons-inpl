0aa45220463707937bc7b982cfd65d3e4eb6c174
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-27 10:59:23 +0100

    bug singleton

--- sale_inplast/models/sale_order.py
+++ sale_inplast/models/sale_order.py
@@ -61,8 +61,9 @@ class SaleOrder(models.Model):
                 pppackaging = self.env['product.product'].search(
                     [('pnt_parent_id', '=', li.product_id.product_tmpl_id.id),
                      ('pnt_parent_qty', '=', li.product_packaging_id.qty),
-                     ('mrp_bom_template_id','=',li.product_packaging_id.mrp_bom_template_id.id)
-                     ])
+                     ('mrp_bom_template_id','=',li.product_packaging_id.mrp_bom_template_id.id),
+                     ('pnt_product_type','=','packing')
+                     ], limit=1)
                 mrppackaging = self.env['product.packaging'].search(
                     [('product_id','=',pppackaging.id),('qty','=',1)])
                 if pppackaging.id and mrppackaging.id:
