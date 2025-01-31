383caef3d4a53fd943b8a46ef8b62b566e4dc0c6
amunozmt <amunoz@puntsistemes.es>
2025-01-31 10:56:53 +0000

    error attachments

--- report_inplast/models/stock_picking.py
+++ report_inplast/models/stock_picking.py
@@ -15,5 +15,6 @@ class StockPicking(models.Model):
             if parent_lot != line.lot_id.parent_id.name:
                 parent_lot = line.lot_id.parent_id.name
                 lines.append(line)
+        print(lines)
         return lines
 
