2ec7e76c330a3a55acd8d7e0447a7d94553bfd17
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-02-04 10:36:37 +0100

    método mig_inventario

--- custom_inplast/models/mig_inventario.py
+++ custom_inplast/models/mig_inventario.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models, _
-
+from odoo.exceptions import UserError
 
 class MigInventario(models.Model):
     _name = 'mig.inventario'
@@ -36,18 +36,18 @@ class MigInventario(models.Model):
 
             # Busco la ubicación:
             locationname = r.ubicacion.strip() + " (" + r.ubicacion_old.strip() + ")"
-            location = env['stock.location'].search([('name', '=', locationname)])
+            location = self.env['stock.location'].search([('name', '=', locationname)])
             if len(location.ids) > 1: raise UserError('Varias ubicaciones con el mismo nombre: ' + locationname)
 
             # Busco el producto:
             productname = r.name.strip()
-            product = env['product.product'].search([('default_code', '=', productname)])
+            product = self.env['product.product'].search([('default_code', '=', productname)])
             if len(product.ids) > 1: raise UserError('Varios productos con la misma referencia: ' + productname)
 
             # Crear lotes si no existen y actualizar inventario:
             if product.id and location.id and not r.sml_id.id:
                 nombrelote = r.lote + "/" + r.palet
-                lot = env['stock.lot'].search([('name', '=', nombrelote), ('product_id', '=', product.id)])
+                lot = self.env['stock.lot'].search([('name', '=', nombrelote), ('product_id', '=', product.id)])
                 if lot.ids:
                     lote = lot[0].id
                     # Cambiado 15/Enero/25 para considerar lotes que ya existen:
@@ -55,30 +55,30 @@ class MigInventario(models.Model):
                 else:
                     ssccs, parentlotname = [], False
                     parentlotname = nombrelote.split(".")
-                    parentlot = env['stock.lot'].search(
+                    parentlot = self.env['stock.lot'].search(
                         [('name', '=', parentlotname[0]), ('product_id', '=', product.id)])
                     if not parentlot.id and parentlotname:
-                        parentlot = env['stock.lot'].create(
+                        parentlot = self.env['stock.lot'].create(
                             {'name': parentlotname[0], 'mig_fechafabricacion': r.mig_fechafabricacion,
                              'product_id': product.id})
 
                     if r.sscc1:
-                        sscc1 = env['pnt.sscc.code'].create({'name': r.sscc1})
+                        sscc1 = self.env['pnt.sscc.code'].create({'name': r.sscc1})
                         ssccs.append(sscc1.id)
                     if r.sscc2:
-                        sscc2 = env['pnt.sscc.code'].create({'name': r.sscc2})
+                        sscc2 = self.env['pnt.sscc.code'].create({'name': r.sscc2})
                         ssccs.append(sscc2.id)
-                    lote = env['stock.lot'].create(
+                    lote = self.env['stock.lot'].create(
                         {'name': nombrelote, 'mig_fechafabricacion': r.mig_fechafabricacion, 'product_id': product.id,
                          'parent_id': parentlot.id, 'sscc_code_ids': [(6, 0, ssccs)]}).id
 
                 # Procedimiento de inventariado automático:
-                newsm = env['stock.move'].create(
+                newsm = self.env['stock.move'].create(
                     {'name': product.name, 'product_id': product.id, 'location_id': 14, 'location_dest_id': location.id,
                      'product_uom_qty': r.qty, 'company_id': env.company.id, 'state': 'done'})
-                newsml = env['stock.move.line'].create({
+                newsml = self.env['stock.move.line'].create({
                     'product_id': product.id, 'location_id': 14, 'location_dest_id': location.id, 'qty_done': r.qty,
-                    'company_id': env.company.id, 'lot_id': lote, 'move_id': newsm.id, 'state': 'done'})
+                    'company_id': self.env.company.id, 'lot_id': lote, 'move_id': newsm.id, 'state': 'done'})
 
                 # Registrar línea mig_inventario:
                 r.write({'product_id': product.id, 'location_id': location.id, 'lot_id': lote, 'sml_id': newsml.id})
