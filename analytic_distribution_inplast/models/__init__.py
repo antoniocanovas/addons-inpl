fa6aafb0a4fbfe430be6b2d65fe3cb7dcca22360
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-02-11 18:33:22 +0100

    analytic_dist_lines

--- analytic_distribution_inplast/models/__init__.py
+++ analytic_distribution_inplast/models/__init__.py
@@ -2,11 +2,8 @@
 
 from . import analytic_distribution_template
 from . import account_analytic_account
-from . import account_analytic_line
 from . import res_company
-from . import account_move_line
 from . import account_account
-from . import account_analytic_line
 from . import analytic_distribution_parameter
 from . import analytic_distribution
 from . import sale_order_line
--- analytic_distribution_inplast/models/res_company.py
+++ analytic_distribution_inplast/models/res_company.py
@@ -8,18 +8,3 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     analytic_product_plan_id = fields.Many2one('account.analytic.plan', string='Product plan')
-    analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan')
-    default_sale_analytic_distribution_account_id = fields.Many2one(
-        'account.analytic.account',
-        string='Sale department account',
-        help='It will be used on customer invoice lines by default.'
-    )
-
-
-    # Eliminado tras reunión con Ibarra (enero 2025):
-    #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
-    #analytic_department_plan_id = fields.Many2one('account.analytic.plan', string='Department')
-
-    #analytic_spain_account_id = fields.Many2one('account.analytic.account', string='Spain account')
-    #analytic_eu_account_id = fields.Many2one('account.analytic.account', string='Europe account')
-    #analytic_non_eu_account_id = fields.Many2one('account.analytic.account', string='Non Europe account')
