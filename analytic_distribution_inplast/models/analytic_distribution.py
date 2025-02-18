ca5a0d15ca2afc83f73d0a9930175d3d69ab9c4d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-02-18 13:42:10 +0100

    to eng

--- analytic_distribution_inplast/models/analytic_distribution.py
+++ analytic_distribution_inplast/models/analytic_distribution.py
@@ -471,7 +471,7 @@ class AnalyticDistribution(models.Model):
             cardboard_unload = parameters.raw_cardboard_unload * rec.picking_in_cardboard_qty
             bag_unload = parameters.raw_bag_unload * rec.picking_in_bag_qty
             pallet_unload = parameters.raw_pallet_unload * rec.picking_in_pallet_qty
-            internal_pickings = rec.days * (parameters.raw_color_relocation_daily + parameters.raw_cardboard_relocation_daily + parameters.raw_bag_relocation_daily + parameters.raw_pallet_relocation_daily)
+            internal_pickings = rec.days * (parameters.raw_color_reloc_daily + parameters.raw_cardboard_reloc_daily + parameters.raw_bag_reloc_daily + parameters.raw_pallet_reloc_daily)
             container_load = rec.sale_container_qty * parameters.container_load
 
             rec.picking_hour_qty = cistern_unload + sack_unload + color_unload + cardboard_unload + bag_unload + pallet_unload + internal_pickings + container_load
